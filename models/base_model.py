b44948fb57b4edad277c637c0ca0de16683c6681
HichamZineb <hichamelamine@hotmail.com>
2023-08-13 03:04:19 -0700

    update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -3,6 +3,7 @@
 
 import uuid
 from datetime import datetime
+import models
 
 
 class BaseModel:
@@ -13,17 +14,18 @@ class BaseModel:
 
         if kwargs:
             for key, value in kwargs.items():
-                if key != '__class__':
-                    if key == 'created_at' or key == 'updated_at':
-                        date_format = "%Y-%m-%dT%H:%M:%S.%f"
-                        value = datetime.strptime(value, date_format)
-                        setattr(self, key, value)
-                    else:
-                        setattr(self, key, value)
-        else:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
+                if key == '__class__':
+                    continue
+                if key == "created_at" or key == "updated_at":
+                    value = datetime.fromisoformat(value)
+                setattr(self, key, value)
+            return
+
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+        models.storage.new(self)
 
     def __str__(self):
         """ Return a string representation """
@@ -35,6 +37,7 @@ class BaseModel:
         """ Save method """
 
         self.updated_at = datetime.now()
+        models.storage.save()
 
     def to_dict(self):
         """ Return a dictionnary """
